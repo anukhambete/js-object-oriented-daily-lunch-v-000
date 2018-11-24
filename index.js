67564f33e1dfeb8462ea2130278461797f1bc58c
Anu Khambete <anukhambete@gmail.com>
2018-11-24 20:50:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 let neighborhoodId = 0;
 let customerId = 0;
+let mealId = 0;
+
 class Neighborhood{
   constructor(name){
     this.id = ++neighborhoodId;
@@ -15,5 +17,13 @@ class Customer{
     this.id = ++customerId;
     this.name = name;
     this.neighborhoodId = neighborhoodId;
+  }
+}
+
+class Meal{
+  constructor(title, price){
+    this.id = ++mealId;
+    this.title = title;
+    this.price = price;
   }
 }
