830ed2dbc89b9f5b79e820b0f0fb9249b1d4c0ca
Anu Khambete <anukhambete@gmail.com>
2018-11-24 20:45:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
-
+let neighborhoodId = 0;
 class Neighborhood{
-  constructor(name)
+  constructor(name){
+    this.id = ++neighborhoodId;
+    this.name = name;
+  }
 }
